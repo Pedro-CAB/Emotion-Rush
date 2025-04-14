28042e5d796452d328841cb4202181df8bb5fc94
Pedro Gomes <80784137+Pedro-CAB@users.noreply.github.com>
2025-04-14 16:38:59 +0100

    Basic Linear Dialog Box Logic

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent; // Reference to the TextMeshProUGUI component for displaying text
+    public string[] lines; // Array of strings to hold the dialogue lines
+    public float textSpeed; // Speed at which the text is displayed
+    private int index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    void StartDialogue(){
+        index = 0;
+        StartCoroutine(TypeLine()); // Start the coroutine to type out the first line of dialogue
+    }
+
+    void NextLine(){
+        if (index < lines.Length - 1){
+            index++;
+            textComponent.text = string.Empty; // Clear the text component before displaying the next line
+            StartCoroutine(TypeLine()); // Start the coroutine to type out the next line of dialogue
+        }
+        else{
+            gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator TypeLine(){
+        foreach (char letter in lines[index].ToCharArray()){
+            textComponent.text += letter; // Display each letter one by one
+            yield return new WaitForSeconds(textSpeed); // Wait for the specified text speed before displaying the next letter
+        }
+    }
+
+    public void Pressed(){
+        if (textComponent.text == lines[index]){ //If previous line is already fully displayed
+            NextLine();
+        }
+        else{
+            StopAllCoroutines(); // Stop the typing coroutine if the button is pressed before the line is fully displayed
+            textComponent.text = lines[index]; // Display the full line immediately
+        }
+        Debug.Log("Line Skipped!");
+
+    }
+}
