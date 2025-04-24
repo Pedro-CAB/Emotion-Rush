70fe9d91980ac14e78df7da413f3e491dfeaddd0
Pedro Gomes <80784137+Pedro-CAB@users.noreply.github.com>
2025-04-24 18:25:38 +0100

    Main Menu Buttons Working

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame(){
+        Debug.Log("New Game Started");
+    }
+
+    public void LoadGame(){
+        Debug.Log("Load Game Started");
+    }
+
+    public void QuitGame(){
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+
+    public void Options(){
+        Debug.Log("Options Menu Opened");
+    }
+}
