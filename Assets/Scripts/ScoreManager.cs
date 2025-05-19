49fc98138603a632a446e130cb894af729d55d12
Pedro Gomes <80784137+Pedro-CAB@users.noreply.github.com>
2025-05-19 21:39:01 +0100

    Daily Outcome Scene

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
+    public bool isOutcomeMenu; //Set as true in the outcome menu scene
+
+    // Scores and Increments for Each Class
     int classAScore;
+    int playerScoreIncrement; // Daily Score Increment based on player choices
     int classBScore;
-
+    int classBIncrement;
     int classCScore;
-
+    int classCIncrement;
     int classDScore;
+    int classDIncrement;
 
+    // Values for UI Positioting
+    //int firstPositionY = 140;
+    //int secondPositionY = 70;
+    //int thirdPositionY = 0;
+    //int fourthPositionY = -70;
+
+    public Text classAIncText;
+    public Text classBIncText;
+    public Text classCIncText;
+    public Text classDIncText;
+
+    public Image classABar;
+    public Image classBBar;
+    public Image classCBar;
+    public Image classDBar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,25 +38,75 @@ public class ScoreManager : MonoBehaviour
         classBScore = PlayerPrefs.GetInt("classBScore");
         classCScore = PlayerPrefs.GetInt("classCScore");
         classDScore = PlayerPrefs.GetInt("classDScore");
+        updateDisplayedResults();
     }
 
-    void incrementScore(string className, int score)
+    void updateDisplayedResults()
     {
-        switch (className)
+        randomizeOtherClassIncrements();
+        updateIncs();
+        updateBars();
+        saveScores();
+        resetIncrements();
+    }
+
+
+    void updateBars()
+    {
+        classABar.fillAmount = (float)classAScore / 70f; //70 Points is the max score possible per week
+        classBBar.fillAmount = (float)classBScore / 70f;
+        classCBar.fillAmount = (float)classCScore / 70f;
+        classDBar.fillAmount = (float)classDScore / 70f;
+    }
+    void updateIncs()
+    {
+        classAIncText.text = displayIncAsString(playerScoreIncrement);
+        classBIncText.text = displayIncAsString(classBIncrement);
+        classCIncText.text = displayIncAsString(classCIncrement);
+        classDIncText.text = displayIncAsString(classDIncrement);
+    }
+
+    string displayIncAsString(int inc)
+    {
+        if (inc > 0)
         {
-            case "A":
-                classAScore += score;
-                break;
-            case "B":
-                classBScore += score;
-                break;
-            case "C":
-                classCScore += score;
-                break;
-            case "D":
-                classDScore += score;
-                break;
+            return "+" + inc.ToString();
         }
+        else if (inc < 0)
+        {
+            return inc.ToString();
+        }
+        else
+        {
+            return "0";
+        }
+    }
+
+    void incrementPlayerScore(int score)
+    {
+        playerScoreIncrement += score;
+        PlayerPrefs.SetInt("playerScoreIncrement", playerScoreIncrement);
+    }
+
+    void resetIncrements()
+    {
+        playerScoreIncrement = 0;
+        classBIncrement = 0;
+        classCIncrement = 0;
+        classDIncrement = 0;
+    }
+
+    void randomizeOtherClassIncrements()
+    {
+        classBIncrement = Random.Range(-3, 6);
+        classCIncrement = Random.Range(-3, 6);
+        classDIncrement = Random.Range(-3, 6);
+        Debug.Log("Class B Increment: " + classBIncrement);
+        Debug.Log("Class C Increment: " + classCIncrement);
+        Debug.Log("Class D Increment: " + classDIncrement);
+        classBScore = Mathf.Clamp(classBScore + classBIncrement, 0, 70);
+        classCScore = Mathf.Clamp(classCScore + classCIncrement, 0, 70);
+        classDScore = Mathf.Clamp(classDScore + classDIncrement, 0, 70);
     }
 
     void saveScores()
@@ -44,5 +115,7 @@ public class ScoreManager : MonoBehaviour
         PlayerPrefs.SetInt("classBScore", classBScore);
         PlayerPrefs.SetInt("classCScore", classCScore);
         PlayerPrefs.SetInt("classDScore", classDScore);
+        playerScoreIncrement = 0;
+        PlayerPrefs.SetInt("playerScoreIncrement", playerScoreIncrement);
     }
 }
