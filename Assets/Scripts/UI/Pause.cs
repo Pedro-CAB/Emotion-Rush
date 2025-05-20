9fd62a5541e92a563ca27a98f4cd1f1decf0f3e2
Pedro Gomes <80784137+Pedro-CAB@users.noreply.github.com>
2025-05-20 17:51:49 +0100

    Refactoring Pause Logic + Are You Sure Menu

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -7,10 +7,13 @@ public class Pause : MonoBehaviour
     public GameObject pausedUI;
     public GameObject unpausedUI;
 
+    public GameObject areYouSureUI;
+
     void Start()
     {
         pausedUI.SetActive(false);
         unpausedUI.SetActive(true);
+        areYouSureUI.SetActive(false);
     }
 
     public void ResumeGame(){
@@ -27,7 +30,20 @@ public class Pause : MonoBehaviour
         unpausedUI.SetActive(false);
     }
 
-    public void QuitGame(){
+    public void AreYouSure()
+    {
+        areYouSureUI.SetActive(true);
+        pausedUI.SetActive(false);
+    }
+
+    public void NotSure()
+    {
+        areYouSureUI.SetActive(false);
+        pausedUI.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
         Debug.Log("Back to Main Menu");
         SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1;
