8d2e4b727285fcf2d3e9befd41fcc489fa7bec4a
Pedro Gomes <80784137+Pedro-CAB@users.noreply.github.com>
2025-05-25 16:34:07 +0100

    Fixed Dialogue Transitions but Interaction End is Bugging

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -14,38 +14,13 @@ public class DialogueManager : MonoBehaviour
 
     public void Start()
     {
-        linearDialogueBox.enabled = false; // Disable the linear dialogue box by default
-        twoOptionDialogueBox.enabled = false; // Disable the two option dialogue box by default
-        threeOptionDialogueBox.enabled = false; // Disable the three option dialogue box by default
+        linearDialogueBox.gameObject.SetActive(false); // Disable the linear dialogue box by default
+        twoOptionDialogueBox.gameObject.SetActive(false); // Disable the two option dialogue box by default
+        threeOptionDialogueBox.gameObject.SetActive(false); // Disable the three option dialogue box by default
+        
+        //twoOptionDialogueBox.enabled = false; // Disable the two option dialogue box by default
+        //threeOptionDialogueBox.enabled = false; // Disable the three option dialogue box by default
     }
-
-    public void Update()
-    {
-        Debug.Log($"Linear: {linearDialogueBox.enabled}, TwoOption: {twoOptionDialogueBox.enabled}, ThreeOption: {threeOptionDialogueBox.enabled}");
-    }
-
-    /**public void Update()
-    {
-        DialogueLine.LineType lineType = currentLine.type;
-        if (lineType == DialogueLine.LineType.Linear)
-        {
-            linearDialogueBox.DisplayDialogueBox();
-            twoOptionDialogueBox.HideDialogueBox();
-            threeOptionDialogueBox.HideDialogueBox();
-        }
-        else if (lineType == DialogueLine.LineType.TwoOption)
-        {
-            twoOptionDialogueBox.DisplayDialogueBox();
-            linearDialogueBox.HideDialogueBox();
-            threeOptionDialogueBox.HideDialogueBox();
-        }
-        else if (lineType == DialogueLine.LineType.ThreeOption)
-        {
-            threeOptionDialogueBox.DisplayDialogueBox();
-            linearDialogueBox.HideDialogueBox();
-            twoOptionDialogueBox.HideDialogueBox();
-        }
-    }*/
 
     public void setCurrentLine(DialogueLine line, string triggeredBy = null)
     {
@@ -58,32 +33,41 @@ public class DialogueManager : MonoBehaviour
         if (currentLine.type == DialogueLine.LineType.Linear)
         {
             Debug.Log("DialogueManager: Starting linear dialogue:" + currentLine.content);
-            linearDialogueBox.enabled = true; // Ensure the linear dialogue box is enabled
+            /**linearDialogueBox.enabled = true; // Ensure the linear dialogue box is enabled
             twoOptionDialogueBox.enabled = false; // Disable the two option dialogue box
-            threeOptionDialogueBox.enabled = false; // Disable the three option dialogue box
+            threeOptionDialogueBox.enabled = false; // Disable the three option dialogue box**/
+            linearDialogueBox.gameObject.SetActive(true); // Show the linear dialogue box
+            twoOptionDialogueBox.gameObject.SetActive(false); // Hide the two option dialogue box
+            threeOptionDialogueBox.gameObject.SetActive(false); // Hide the three option dialogue box
             linearDialogueBox.StartLinearDialogue(currentLine, trigger);
         }
         //Handle two option sequences with the correct GUI interface
         else if (currentLine.type == DialogueLine.LineType.TwoOption)
         {
             Debug.Log("DialogueManager: Starting two option dialogue:" + currentLine.content);
-            twoOptionDialogueBox.enabled = true; // Ensure the two option dialogue box is enabled
+            /**twoOptionDialogueBox.enabled = true; // Ensure the two option dialogue box is enabled
             linearDialogueBox.enabled = false; // Disable the linear dialogue box
-            threeOptionDialogueBox.enabled = false; // Disable the three option dialogue box
+            threeOptionDialogueBox.enabled = false; // Disable the three option dialogue box*/
+            twoOptionDialogueBox.gameObject.SetActive(true); // Show the two option dialogue box
+            linearDialogueBox.gameObject.SetActive(false); // Hide the linear dialogue box
+            threeOptionDialogueBox.gameObject.SetActive(false); // Hide the three option dialogue box
             twoOptionDialogueBox.StartTwoOptionDialogue(currentLine, trigger);
         }
         //Handle three option sequences with the correct GUI interface
         else if (currentLine.type == DialogueLine.LineType.ThreeOption)
         {
-            threeOptionDialogueBox.enabled = true; // Ensure the three option dialogue box is enabled
+            /**threeOptionDialogueBox.enabled = true; // Ensure the three option dialogue box is enabled
             linearDialogueBox.enabled = false; // Disable the linear dialogue box
-            twoOptionDialogueBox.enabled = false; // Disable the two option dialogue box
+            twoOptionDialogueBox.enabled = false; // Disable the two option dialogue box*/
+            threeOptionDialogueBox.gameObject.SetActive(true); // Show the three option dialogue box
+            linearDialogueBox.gameObject.SetActive(false); // Hide the linear dialogue box
+            twoOptionDialogueBox.gameObject.SetActive(false); // Hide the two option dialogue box
             threeOptionDialogueBox.StartThreeOptionDialogue(currentLine, trigger);
         }
     }
 
     public bool isDialogueActive()
     {
-        return linearDialogueBox.enabled || twoOptionDialogueBox.enabled || threeOptionDialogueBox.enabled;
+        return linearDialogueBox.gameObject.activeSelf || twoOptionDialogueBox.gameObject.activeSelf || threeOptionDialogueBox.gameObject.activeSelf;
     }
 }
